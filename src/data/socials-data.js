41e5efacc98954f2b8f87e097bbbab015bd2b5dc
Onche Austin <oncheaustin@gmail.com>
2024-02-01 21:10:23 +0100

    upadate

--- src/data/socials-data.js
+++ src/data/socials-data.js
@@ -4,6 +4,6 @@ export const socialsData = {
     linkedIn: 'https://www.linkedin.com/in/onche-austin-tech/',
     twitter: 'https://twitter.com/abujasenator',
     medium: 'https://medium.com/@austinbelentra',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    stackOverflow: 'https://stackoverflow.com/users/9860399/onche-austin',
     mail: 'oncheaustin@gmail.com',
 }
